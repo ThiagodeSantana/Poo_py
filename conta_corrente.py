4b233f49b2fce0eee5c643c8587726be3b27af91
THIAGO DE SANTANA NIVALDO <titisantanarodregues@hotmail.com>
2025-11-19 15:47:32 -0300

    banco
    aqui está meu banco ,uma forma de aprendizando sobre o famoso POO

---
+++ conta_corrente.py
@@ -0,0 +1,19 @@
+from .conta import Conta
+
+class ContaCorrente(Conta):
+    def __init__(self, titular, saldo=0,limite=500):
+        super().__init__(titular, saldo)
+        self.limite = limite
+
+    def sacar(self,valor):
+        if valor <= self.saldo + self.limite:
+            self.saldo -=valor
+
+        else:
+            print("Saldo + limite insuficientes.")   
+
+
+    def to_dict(self):
+        d = super().to_dict()
+        d["limite"] = self.limite
+        return d        
